de368ce262a54cfa9e7c3b7cb023345d398dbd24
la5chine <khachinechaker@gmail.com>
2020-04-08 06:24:27 -0400

    customer's orders

--- CM/settings.py
+++ CM/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x6oo*_m*=rtlj+l0a#h(=c!j$kdxac$i%0jw!0z=@wol-k7kq^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
